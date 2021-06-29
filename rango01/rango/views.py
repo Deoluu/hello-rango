a03968f9b90c601dcbf73c02c96b5373a4c766be
deoluu <programwix@gmail.com>
2021-06-29 12:11:35 +0100

    added urls to rango/urls.py and view.py

--- rango01/rango/views.py
+++ rango01/rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says 'Hey there, partner'")
+    return HttpResponse("Rango says 'Hey there, partner'. <br> <a href='/rango/about/'> About </a>")
+
+def about(request):
+    return HttpResponse("Rango says this here is the about page. <br> <a href='/rango/'> Home </a>")
